a7ff582b1f64275351144f37d6d74ad5e2069835
Hilmar Preusse <hille42@web.de>
2023-07-20 18:39:00 +0200

    Import Debian changes 1.3.1-1
    hintview (1.3.1-1) unstable; urgency=medium
    .
      * Initial release. (Closes: #1041668)

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/hintview/issues
+# Bug-Submit: https://github.com/<user>/hintview/issues/new
+# Changelog: https://github.com/<user>/hintview/blob/master/CHANGES
+# Documentation: https://github.com/<user>/hintview/wiki
+# Repository-Browse: https://github.com/<user>/hintview
+# Repository: https://github.com/<user>/hintview.git
